0aef44f0808de3763781d1260a98a7081e1c252c
chembeti sai deeraj <sr283909@gmail.com>
2026-08-16 21:57:50 +0530

    Update Python_quiz_game.py

--- Python_quiz_game.py
+++ Python_quiz_game.py
@@ -1,11 +1,9 @@
-# Simple Quiz Game in Python
 
 score = 0
 
 print("Welcome to Python Quiz Game!")
 print()
 
-# Question 1
 answer = input("1. What is the capital of India? ")
 
 if answer.lower() == "delhi":
@@ -16,7 +14,6 @@ else:
 
 print()
 
-# Question 2
 answer = input("2. Which language is used for AI mostly? ")
 
 if answer.lower() == "python":
@@ -27,7 +24,6 @@ else:
 
 print()
 
-# Question 3
 answer = input("3. How many days are there in a week? ")
 
 if answer == "7":
@@ -37,14 +33,12 @@ else:
     print("Wrong!")
 
 print()
-# Question 4
 answer = input("What is the correct spelling of pyton?")
 if answer.lower() == "python"
     print("Correct!")
     score +=1
 esle:
     print("Wrong")
-# Question 5
 answer = input("How many months in a year?")
 if answer.lower() == "12"
     print("Correct!")
@@ -52,6 +46,5 @@ if answer.lower() == "12"
 esle:
     print("Wrong")
 
-# Final Score
 print("Quiz Finished!")
 print("Your score is:", score, "/ 5")
